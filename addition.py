d14ad00a30e80275757fec9caa21da0665b58cc0
Pakhi-21 <pakhigupta556@gmail.com>
2025-02-18 16:57:46 +0530

    Addition file Added

---
+++ addition.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Performing addition
+result = num1 + num2
+
+# Displaying the result
+print("The sum : ",+result)
